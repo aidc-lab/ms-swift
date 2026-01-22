688145974b6d21d45126a052f2527d8352841aa7
luoru <luoru@4paradigm.com>
2026-01-22 16:58:34 +0800

    [feat] add dockerfile.

--- setup.py
+++ setup.py
@@ -15,9 +15,14 @@ version_file = 'swift/version.py'
 
 
 def get_version():
+    # with open(version_file, 'r', encoding='utf-8') as f:
+    #     exec(compile(f.read(), version_file, 'exec'))
+    # return locals()['__version__']
+    version_dict = {}
     with open(version_file, 'r', encoding='utf-8') as f:
-        exec(compile(f.read(), version_file, 'exec'))
-    return locals()['__version__']
+        exec(compile(f.read(), version_file, 'exec'), version_dict)
+    return version_dict['__version__']
+
 
 
 def parse_requirements(fname='requirements.txt', with_version=True):
